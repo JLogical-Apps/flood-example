edb33543c041af9c63db858c750c410f311602bd
jaboyc <jake@jlogical.com>
2024-03-28 11:50:10 -0400

    Remove unecessary code in style.

--- todo/lib/presentation/style.dart
+++ todo/lib/presentation/style.dart
@@ -1,22 +1,7 @@
 import 'package:flood/flood.dart';
 import 'package:flutter/material.dart';
 
-// Style style = FlatStyle(
-//   primaryColor: Color(0xffd97706),
-//   backgroundColor: Color(0xff03111a),
-// );
-
 Style style = FlatStyle(
   primaryColor: Color(0xff287ac5),
   backgroundColor: Color(0xff09090b),
 );
-
-Color getCentsColor(int? amountCents) {
-  if (amountCents == null || amountCents == 0) {
-    return Colors.white;
-  }
-  if (amountCents > 0) {
-    return Colors.green;
-  }
-  return Colors.red;
-}
